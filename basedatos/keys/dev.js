5f741cf910b0dde7430df2f899f7b9ded97ae217
santiago123x <63115057+santiago123x@users.noreply.github.com>
2020-10-23 00:53:08 -0500

    Delete dev.js

--- basedatos/keys/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    posgresqlURI: 'postgres://hqmllkmviedrey:455dcd17d8a5846c455dc75e66d4afd432ae5bbccf7b59d29c8ccd7bdc290083@ec2-184-72-236-3.compute-1.amazonaws.com:5432/dd5mba994afame',
-  };
